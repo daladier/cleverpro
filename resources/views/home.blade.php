44b4ee05ae7aa19b2fcabf1b29b74ffce136b011
daladier <daladier2001@hotmail.com>
2019-03-10 18:15:16 -0300

    Mais
    Mais uma alteração

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'AdminLTE')
+
+@section('content_header')
+    <h1>Painel de Controls</h1>
+@stop
+
+@section('content')
+    <p>Você está conectado!</p>
+@stop
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,5 @@
+@extends('adminlte::page')
+
+@section('content')
+    <p text-align:center><h3>Cleverness|<b>Sistemas</b></h3></p>
+@stop                
